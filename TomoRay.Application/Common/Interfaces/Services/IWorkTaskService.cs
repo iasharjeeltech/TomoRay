8f8326ccc220a04bce06020feaf024abe812f49b
Sharjeel <iasharjeel@gmail.com>
2025-06-08 14:00:17 +0530

    services and its implementation

---
+++ TomoRay.Application/Common/Interfaces/Services/IWorkTaskService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomoRay.Domain.Entities;
+
+namespace TomoRay.Application.Common.Interfaces.Services
+{
+    public interface IWorkTaskService
+    {
+        Task<IEnumerable<WorkTask>> GetAllAsync();
+        Task<WorkTask?> GetByIdAsync(Guid id);
+        Task CreateAsync(WorkTask task);
+        Task UpdateAsync(WorkTask task);
+        Task DeleteAsync(Guid id);
+    }
+}

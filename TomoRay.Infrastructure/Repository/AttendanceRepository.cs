dfd0bb20b18ad338992345533f7f511ff63659ac
Sharjeel <iasharjeel@gmail.com>
2025-06-08 14:03:44 +0530

    interfacesi implementation

---
+++ TomoRay.Infrastructure/Repository/AttendanceRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomoRay.Application.Common.Interfaces;
+using TomoRay.Domain.Entities;
+using TomoRay.Infrastructure.Data;
+
+namespace TomoRay.Infrastructure.Repository
+{
+
+    public class AttendanceRepository : IAttendanceRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendanceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Attendance>> GetAllAsync()
+        {
+            return await _context.Attendances
+                .Include(a => a.User)
+                .ToListAsync();
+        }
+
+        public async Task<Attendance?> GetByIdAsync(Guid id)
+        {
+            return await _context.Attendances
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task AddAsync(Attendance attendance)
+        {
+            await _context.Attendances.AddAsync(attendance);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Attendance attendance)
+        {
+            _context.Attendances.Update(attendance);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var item = await _context.Attendances.FindAsync(id);
+            if (item != null)
+            {
+                _context.Attendances.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}

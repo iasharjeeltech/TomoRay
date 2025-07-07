8a2819d4db2e9186f175a0b356e2e4f97d95733c
Sharjeel <iasharjeel@gmail.com>
2025-07-08 01:11:48 +0530

    UnitOfWork added

---
+++ TomoRay.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomoRay.Application.Common.Interfaces.Services;
+
+namespace TomoRay.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        public IAttendanceRepository AttendanceRepositoryUOW { get; }
+        public ITaskAssignmentRepository TaskAssignmentRepositoryUOW { get; }
+        public IUserRepository UserRepositoryUOW { get; }
+        public IWorkTaskRepository WorkTaskRepositoryUOW { get; }
+
+        //service repo
+        public IWorkTaskService WorkTaskServiceUOW { get; }    
+        public IUserService UserServiceUOW { get; }
+        public IAttendanceService AttendanceServiceUOW { get; }
+    }
+}
---
+++ TomoRay.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomoRay.Application.Common.Interfaces;
+using TomoRay.Application.Common.Interfaces.Services;
+using TomoRay.Infrastructure.Data;
+using TomoRay.Infrastructure.Services;
+
+namespace TomoRay.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IAttendanceRepository AttendanceRepositoryUOW { get; private set; }
+
+        public ITaskAssignmentRepository TaskAssignmentRepositoryUOW { get; private set; }
+
+        public IUserRepository UserRepositoryUOW { get; private set; }
+
+        public IWorkTaskRepository WorkTaskRepositoryUOW { get; private set; }
+
+        public IWorkTaskService WorkTaskServiceUOW { get; private set; }
+
+        public IUserService UserServiceUOW { get; private set; }
+
+        public IAttendanceService AttendanceServiceUOW { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+
+            AttendanceRepositoryUOW = new AttendanceRepository(_db);
+            TaskAssignmentRepositoryUOW = new TaskAssignmentRepository(_db);
+            UserRepositoryUOW = new UserRepository(_db);
+            WorkTaskRepositoryUOW = new WorkTaskRepository(_db);
+
+            WorkTaskServiceUOW = new WorkTaskService(WorkTaskRepositoryUOW, _db);
+            UserServiceUOW = new UserService(UserRepositoryUOW, _db); // ✅ Yeh sahi hai
+            AttendanceServiceUOW = new AttendanceService(AttendanceRepositoryUOW, _db);
+        }
+
+    }
+}
